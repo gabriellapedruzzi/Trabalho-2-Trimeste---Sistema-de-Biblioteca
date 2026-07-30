d0d99ee1da16fe21ebdc91de32d13d640045e0d4
gabriellapedruzzi <gabriellapedruzzi@gmail.com>
2026-07-30 20:53:04 -0300

    8

--- sistema_de_biblioteca.py
+++ sistema_de_biblioteca.py
@@ -1,6 +1,6 @@
 livros = [
-    ["Uma janela sombria", "Rachel Gillig", "978-65-85348-55-3", 1 ],
-    ["Mistborn", "Brandon Sanderson", "978-65-81339-18-0", 1]
+    ["Uma janela sombria", "Rachel Gillig", "978-65-85348-55-3", 3 ],
+    ["Mistborn", "Brandon Sanderson", "978-65-81339-18-0", 5]
 ]
 
 usuarios = {
@@ -12,7 +12,7 @@ def cadastrar_livros():
     while True:
         escolha = input("você deseja cadastrar um livro novo(s/n): ").lower
 
-        if escolha == "s":
+        if escolha == "s" or escolha == "sim":
             titulo = input("Insira o titulo do livro: ")
             autor = input("Insira o nome do autor do livro: ")
             codigo = input("Insira o codigo ISBN do livro: ")
@@ -22,7 +22,7 @@ def cadastrar_livros():
             livros.append(novo_livro)
             print(novo_livro)
             print(livros)
-        elif escolha == "n":
+        elif escolha == "n" or escolha == "não":
             break
         else:
             print("Insira s ou n")
@@ -31,13 +31,13 @@ def cadastrar_livros():
 def cadastrar_usuarios(usuarios):
     while True:
         alternativa = input("Você deseja cadastrar um novo usuario(s/n): ").lower
-        if alternativa == "s":
+        if alternativa == "s" or alternativa == "sim":
             email = input("Insira seu email: ")
             senha = input("insira sea senha: ")
 
             usuarios[email] = senha
             print(usuarios)
-        elif alternativa == "n":
+        elif alternativa == "n" or alternativa == "não":
             break
         else:
             print("insira s ou n")
