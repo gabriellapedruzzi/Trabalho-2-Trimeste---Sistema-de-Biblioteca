e5768cf2fc442f16cbfcc94ac609e6babdef87c2
gabriellapedruzzi <gabriellapedruzzi@gmail.com>
2026-07-31 19:15:24 -0300

    14

--- sistema_de_biblioteca.py
+++ sistema_de_biblioteca.py
@@ -19,7 +19,7 @@ livros = [
 
 revistas = [
     {
-        "id": 2,
+        "id": 8987878,
         "titulo": "National Geographic Brasil",
         "edicao": "284",
         "publicacao": "09/2024",
@@ -28,7 +28,16 @@ revistas = [
     }
 ]
 
-artigos = []
+artigos = [
+    {
+        "id": 6586586, 
+        "titulo": "A Importância da Libras para Inclusão Escolar do Surdo",
+        "autor": "Marilene Domanovski, Adriane Meyer Vassão",
+        "palavras_chave": "Libras, Inclusão Escolar, Surdez",
+        "publicacao": 2016,
+        "quantidade": 3
+    }
+]
 
 usuarios = {
     "gabi@gmail.com": "05062010",
@@ -88,7 +97,7 @@ def cadastrar_revista():
                 id = int(input("Insira o ID: "))
                 titulo = input("Insira o titulo: ")
                 edicao = input("Insira a edição: ")
-                publicacao = input("Insira a data de publicação")
+                publicacao = input("Insira a data de publicação: ")
                 editora = input("Insira a editora da revista: ")
                 quantidade = int(input("Insira a quantidade: "))
 
@@ -110,7 +119,31 @@ def cadastrar_revista():
                 print("Insira s ou n")
 
 def cadastrar_artigo():
-    pass
+    while True:
+            escolha = input("você deseja cadastrar um artigo cíentifico novo(s/n): ").lower()
+    
+            if escolha == "s" or escolha == "sim":
+                titulo = input("Insira o titulo do artigo: ")
+                autor = input("Insira o nome do autores do artigo: ")
+                palavras_chave = input("Insira as palavras chaves: ")
+                publicacao = input("Insira a data de publicação: ")
+                quantidade = int(input("Insira a quantidade de artigos: "))
+                
+                novo_artigo = {
+                    "titulo": titulo,
+                    "autor": autor,
+                    "palavras_chave": palavras_chave,
+                    "publicacao": publicacao,
+                    "quantidade": quantidade
+                }
+    
+                artigos.append(novo_artigo)
+                print(artigos)
+            elif escolha == "n" or escolha == "não" or escolha == "nao":
+                limpar_terminal()
+                break
+            else:
+                print("Insira s ou n")
 
 def mostrar_usuarios(usuarios):
     limpar_terminal()
@@ -122,24 +155,42 @@ def mostrar_usuarios(usuarios):
 def emprestimo_devolucao():
     pass
 
-def consultar_acervo(livros,revistas):
+def consultar_acervo(livros,revistas,artigos):
     limpar_terminal()
     print("LIVROS")
     for livro in livros:
         print(
-        f"Título: {livro['titulo']:<25}"
-        f"Autor(a): {livro['autor']:<30}"
-        f"Genero Literario: {livro['genero']:<20}"
-        f"Codigo ISBN: {livro['codigo']:<27}"
-        f"Quantidade: {livro['quantidade']}"
+            f"° Título: {livro['titulo']:<25}"
+            f"Autor(a): {livro['autor']:<30}"
+            f"Genero Literario: {livro['genero']:<20}"
+            f"Codigo ISBN: {livro['codigo']:<27}"
+            f"Quantidade: {livro['quantidade']}"
     )
 
     print()
+
     print("REVISTAS")
     for revista in revistas:
         print(
-            f"id: {revista['id']:<25}"
-            
+            f"° ID: {revista['id']:<15}"
+            f"Titulo: {revista['titulo']:<20}"
+            f"Edição: {revista['edicao']:<20}"
+            f"Publicação: {revista['publicacao']:<20}"
+            f"Editora: {revista['editora']:<20}"
+            f"Quantidade: {revista['quantidade']}"
+        )
+
+    print()
+
+    print("ARTIGOS CÍENTIFICOS")
+    for artigo in artigos:
+        print(
+            f"° ID: {artigo['id']:<12}"
+            f"Titulo: {artigo['titulo']:<60}"
+            f"Autoes: {artigo['autor']:<45}"
+            f"Palavras-chaves: {artigo['palavras_chave']:<37}"
+            f"Publicação: {artigo['publicacao']:<10}"
+            f"Quantidade: {artigo['quantidade']}"
         )
         
 def relatorio():
@@ -172,7 +223,7 @@ while True:
     elif opcao == "5":
         emprestimo_devolucao()
     elif opcao == "6":
-        consultar_acervo(livros,revistas)
+        consultar_acervo(livros,revistas,artigos)
     elif opcao == "7":
         mostrar_usuarios(usuarios)
     elif opcao == "8":
